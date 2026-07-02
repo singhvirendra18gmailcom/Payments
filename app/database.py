a83aa4fe0f3e2330a654deef72feb088fa34f0ad
singhvirendra18gmailcom <singh.virendra18@gmail.com>
2026-07-02 10:06:42 +0530

    db file should be at root project location.

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from pathlib import Path
 
-DATABASE_URL = "sqlite:///./payment_assistant.db"
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = BASE_DIR / "payment_assistant.db"
+
+DATABASE_URL = f"sqlite:///{DB_PATH}"
+
 
 engine = create_engine(
     DATABASE_URL,
