b3f136611a0b0591fb23760ba5540ac8be298534
singhvirendra18gmailcom <singh.virendra18@gmail.com>
2026-07-09 07:10:36 +0530

    "#32 AI PROVIDER CONFIGURATION"

--- app/config.py
+++ app/config.py
@@ -25,12 +25,17 @@ DATABASE_URL = os.getenv(
     "sqlite:///./payment_assistant.db"
 )
 
-LLM_AI_PROVIDER = os.getenv(
-    "AI_KEY",
+AI_PROVIDER = os.getenv(
+    "AI_PROVIDER",
     "gemini"
 )
 
-LLM_API_KEY = os.getenv(
+GEMINI_API_KEY = os.getenv(
     "GEMINI_API_KEY",
     "test_api_key_for_local_and_ci"
+)
+
+GEMINI_MODEL = os.getenv(
+    "GEMINI_MODEL",
+    "gemini-2.0-flash"
 )
